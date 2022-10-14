1e0a9837f11d89fae2d055b486a1a1b98baf15ac
midnightgizmo <scottamillard@hotmail.com>
2022-10-14 12:37:01 +0100

    Add Database CRUD for Invoice

---
+++ InvoiceAssistantV2/InvoiceAssistantV2/Database/DatabaseInteraction.cs
@@ -0,0 +1,38 @@
+﻿using Database.DbInteractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database
+{
+    public class DatabaseInteraction
+    {
+        private Data.InvoiceAssistantDbContext _DbContext;
+        public DatabaseInteraction()
+        {
+            _DbContext = new Data.InvoiceAssistantDbContext();
+        }
+
+        private InvoiceDb _Invoices = null!;
+        public InvoiceDb Invoices
+        {
+            get
+            {
+                if (this._Invoices == null)
+                    this._Invoices = new InvoiceDb(this._DbContext);
+
+                return this._Invoices;
+            }
+        }
+
+        /// <summary>
+        /// Will updated the database with any CRUD operations we have performed
+        /// </summary>
+        public void SaveChanges()
+        {
+            this._DbContext.SaveChanges();
+        }
+    }
+}
---
+++ InvoiceAssistantV2/InvoiceAssistantV2/Database/DbInteractions/InvoiceDb.cs
@@ -0,0 +1,84 @@
+﻿using InvoiceAssistantV2.Shared.Models.Database.Invoice;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database.DbInteractions
+{
+    public class InvoiceDb
+    {
+        private Data.InvoiceAssistantDbContext _DbContext;
+        public InvoiceDb(Data.InvoiceAssistantDbContext dbContext)
+        {
+            _DbContext = dbContext;
+        }
+
+        public List<Invoice> SelectAll()
+        {
+            return this._DbContext.Invoices.ToList();
+        }
+
+        public Invoice? SelectInvoice(int InvoiceID)
+        {
+            Invoice invoice = new Invoice() { Id=InvoiceID };
+
+            return this._DbContext.Invoices.FirstOrDefault(I => I.Id == InvoiceID);
+        }
+
+        public List<Invoice> Select_CustomQuery(DateTime? StartDate, DateTime? EndDate,
+                                                decimal? StartAmmount, decimal? EndAmmount,
+                                                DateTime? DateRecievedMoneyStart, DateTime? DateRecievedMoneyEnd,
+                                                string? ReferenceNumber, int? TypeOfPaymentId, int? AddressToMakePaymentOutToId,
+                                                string? Description)
+        {
+            IQueryable<Invoice> query = this._DbContext.Invoices;
+            
+            if (StartDate != null)
+                query = query.Where(I => I.DateOfInvoice >= StartDate);
+            if (EndDate != null)
+                query = query.Where(I => I.DateOfInvoice <= EndDate);
+
+            if(StartAmmount != null)
+                query = query.Where(I => I.TotalInvoiceAmmount >= StartAmmount);
+            if(EndAmmount != null)
+                query = query.Where(I => I.TotalInvoiceAmmount <= EndAmmount);
+
+            if (DateRecievedMoneyStart != null)
+                query = query.Where(I => I.DateRecievedMoney >= DateRecievedMoneyStart);
+            if (DateRecievedMoneyEnd != null)
+                query = query.Where(I => I.DateRecievedMoney <= DateRecievedMoneyEnd);
+
+            if(ReferenceNumber != null)
+                query = query.Where(I => I.ReferenceNumber.Contains(ReferenceNumber, StringComparison.OrdinalIgnoreCase));
+
+            if (TypeOfPaymentId != null)
+                query = query.Where(I => I.PaymentTypeID == TypeOfPaymentId);
+
+            if(AddressToMakePaymentOutToId != null)
+                query = query.Where(I => I.AddressToMakeInvoiceOutToId == AddressToMakePaymentOutToId);
+
+            if(Description != null)
+                query = query.Where(I => I.Description.Contains(Description,StringComparison.OrdinalIgnoreCase));
+
+            return query.ToList();
+
+        }
+        public void Update(Invoice InvoiceToUpdate)
+        {
+            this._DbContext.Update(InvoiceToUpdate);
+        }
+
+        public void Insert(Invoice NewInvoice)
+        {
+            this._DbContext.Add(NewInvoice);
+        }
+
+        public void Delete(Invoice InvoiceToDelete)
+        {
+            this._DbContext.Remove(InvoiceToDelete);
+        }
+        
+    }
+}
