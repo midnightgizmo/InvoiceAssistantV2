16ccf87dcc9af57e5f8ae9cf95c86a0d4b9b0713
midnightgizmo <scottamillard@hotmail.com>
2022-10-20 11:23:13 +0100

    Client: Add Serach Invoice Page
    Created Search invoice page.
    Still need to add code to post back to the server.

---
+++ InvoiceAssistantV2/InvoiceAssistantV2/InvoiceAssistantV2/Client/ViewModels/Invoices/SearchInvoiceVM.cs
@@ -0,0 +1,13 @@
+﻿using InvoiceAssistantV2.Shared.Models;
+using InvoiceAssistantV2.Shared.Models.Database.Invoice;
+
+namespace InvoiceAssistantV2.Client.ViewModels.Invoices
+{
+	public class SearchInvoiceVM
+	{
+        public InvoiceSearchParameters FormModel { get; set; } = new InvoiceSearchParameters();
+
+
+        public List<PaymentType> PaymentTypes { get; set; }
+    }
+}
---
+++ InvoiceAssistantV2/InvoiceAssistantV2/InvoiceAssistantV2/Shared/Models/InvoiceSearchParameters.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvoiceAssistantV2.Shared.Models
+{
+    /// <summary>
+    /// All the posible search parameters we can use when searching for invoices
+    /// </summary>
+	public class InvoiceSearchParameters
+	{
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public decimal? StartAmmount { get; set; }
+        public decimal? EndAmmount { get; set; }
+        public DateTime? DateRecievedMoneyStart { get; set; }
+        public DateTime? DateRecievedMoneyEnd { get; set; }
+        public string? ReferenceNumber { get; set; }
+        public int? TypeOfPaymentId { get; set; } 
+        public int? AddressToMakePaymentOutToId { get; set; }
+        public string? Description { get; set; }
+
+    }
+}
