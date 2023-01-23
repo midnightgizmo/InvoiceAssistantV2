7ae78f9b9a64ba3ffad8993acd2357e5b6c56981
midnightgizmo <scottamillard@hotmail.com>
2023-01-23 12:49:51 +0000

    Implemented IEditableObject on Invoice Model

--- InvoiceAssistantV2/InvoiceAssistantV2/InvoiceAssistantV2/Shared/Models/Database/Invoice/Invoice.cs
+++ InvoiceAssistantV2/InvoiceAssistantV2/InvoiceAssistantV2/Shared/Models/Database/Invoice/Invoice.cs
@@ -1,6 +1,7 @@
 ﻿using InvoiceAssistantV2.Shared.Models.Database.Company;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,8 @@ using System.Threading.Tasks;
 
 namespace InvoiceAssistantV2.Shared.Models.Database.Invoice
 {
-    public class Invoice
-    {
+    public class Invoice : IEditableObject
+	{
         [Key]
         public int Id { get; set; }
         /// <summary>
@@ -53,5 +54,63 @@ namespace InvoiceAssistantV2.Shared.Models.Database.Invoice
         /// List of payments that belong to this invoice
         /// </summary>
         public List<InvoicePaymentBreakDown> InvoicePayments { get; set; } = null!;
-    }
+
+
+
+
+        private Invoice _InvoiceCopy;
+
+
+        /// <summary>
+        /// Does NOT copy <see cref="PlacesVisitedForInvoice"/> or <see cref="InvoicePayments"/>
+        /// </summary>
+        /// <returns></returns>
+		public static Invoice Copy(Invoice CopyFrom, Invoice CopyTo)
+		{
+            // make a copy of all the data except PlacesVisitedForInvoice and InvoicePayments (just
+            // ignore those properties)
+
+            CopyTo.Id = CopyFrom.Id;
+            CopyTo.ReferenceNumber = CopyFrom.ReferenceNumber;
+            CopyTo.Description = CopyFrom.Description;
+            CopyTo.DateOfInvoice = CopyFrom.DateOfInvoice;
+            CopyTo.DateRecievedMoney = CopyFrom.DateRecievedMoney;
+            CopyTo.TotalInvoiceAmmount = CopyFrom.TotalInvoiceAmmount;
+            CopyTo.PaymentTypeID = CopyFrom.PaymentTypeID;
+            CopyTo.AddressToMakeInvoiceOutToId = CopyFrom.AddressToMakeInvoiceOutToId;
+            CopyTo.PaymentType = CopyFrom.PaymentType;
+            CopyTo.AddressToMakeInvoiceOutTo = CopyFrom.AddressToMakeInvoiceOutTo;
+
+			return CopyTo;
+		}
+
+		public void BeginEdit()
+		{
+			this._InvoiceCopy = Invoice.Copy(this,new Invoice());
+		}
+
+		/// <summary>
+		/// We are happy with the changes we made and now want to commit
+		/// them to memory
+		/// </summary>
+		public void EndEdit()
+		{
+			// no longer need the orignal
+			this._InvoiceCopy = null;
+		}
+
+		/// <summary>
+		/// We want to restore the orignal values and
+		/// forget any changes that were made from when
+		/// BeginEdit was called
+		/// </summary>
+		public void CancelEdit()
+		{
+			// retore the orignal values
+			Invoice.Copy(this._InvoiceCopy, this);
+
+			// no longer need the copy
+			this._InvoiceCopy = null;
+		}
+	}
 }
