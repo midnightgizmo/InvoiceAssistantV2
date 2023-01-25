bb2704e3337255c7365c7604f469bd12a766b8b9
midnightgizmo <scottamillard@hotmail.com>
2023-01-25 12:15:35 +0000

    Fix: Payment total ammount updates correctly
    When a payment was added or removed, the invoice total ammount was not getting updated. This is now fixed.

--- InvoiceAssistantV2/InvoiceAssistantV2/InvoiceAssistantV2/Server/ControllersLogic/Invoice/AddPaymentControllerLogiccs.cs
+++ InvoiceAssistantV2/InvoiceAssistantV2/InvoiceAssistantV2/Server/ControllersLogic/Invoice/AddPaymentControllerLogiccs.cs
@@ -4,6 +4,7 @@ using Database.Data;
 using Database.DbInteractions;
 using InvoiceAssistantV2.Client.ViewModels.Invoices.Add;
 using InvoiceAssistantV2.Shared.Models.Database.Invoice;
+using Microsoft.EntityFrameworkCore;
 
 namespace InvoiceAssistantV2.Server.ControllersLogic.Invoice
 {
@@ -65,7 +66,10 @@ namespace InvoiceAssistantV2.Server.ControllersLogic.Invoice
 			{
 				// make sure the Invoice exists
 				InvoiceDb dbInvoice = new InvoiceDb(dbContext);
-				if(dbInvoice.SelectInvoice(InvoiceId) == null)
+				// find the invoice in the database we want to add a payament too
+				Shared.Models.Database.Invoice.Invoice? FoundInvoice = dbInvoice.SelectInvoice(InvoiceId);
+				
+				if(FoundInvoice == null)
 				{
 					returnValue.HasErrors = true;
 					returnValue.Errors.Add("Invoice not found");
@@ -83,13 +87,29 @@ namespace InvoiceAssistantV2.Server.ControllersLogic.Invoice
 					returnValue.Errors.Add("Unable to add payment to invoice");
 					return returnValue;
 				}
+				// payment was sucsesfully added
 				else
+				{
 					returnValue.ReturnValue = newPayment;
+
+					// Sums up all the payments allocated to this invoice and updates
+					// the TotalInvoiceAmmount value in the Invoice table
+
+					// sum of all the payments allocated to this invoice
+					FoundInvoice.TotalInvoiceAmmount = dbPaymentBreakDown.GetAllPaymentsForInvoice(InvoiceId).Sum(s => s.Ammount);
+					// update the invoice total ammount in the database
+					dbInvoice.Update(FoundInvoice);
+					dbContext.SaveChanges();
+				}
+
+				
 			}
 
+			
 
 			// if we get this far,we should have a new payemnt set in returnValue.Returnvalue;
 			return returnValue;
 		}
+
 	}
 }
--- InvoiceAssistantV2/InvoiceAssistantV2/InvoiceAssistantV2/Server/ControllersLogic/Invoice/RemovePaymentControllerLogic.cs
+++ InvoiceAssistantV2/InvoiceAssistantV2/InvoiceAssistantV2/Server/ControllersLogic/Invoice/RemovePaymentControllerLogic.cs
@@ -1,5 +1,7 @@
 ﻿using Database.Data;
 using Database.DbInteractions;
+using InvoiceAssistantV2.Client.Pages.Invoices;
+using InvoiceAssistantV2.Shared.Models.Database.Invoice;
 
 namespace InvoiceAssistantV2.Server.ControllersLogic.Invoice
 {
@@ -40,16 +42,52 @@ namespace InvoiceAssistantV2.Server.ControllersLogic.Invoice
 
 			using(InvoiceAssistantDbContext dbContext = new InvoiceAssistantDbContext())
 			{
-				InvoicePaymentBreakDownDb dbPaymentBreadkDown = new InvoicePaymentBreakDownDb(dbContext);
+				InvoicePaymentBreakDownDb dbPaymentBreakDown = new InvoicePaymentBreakDownDb(dbContext);
+				// we need to find the payment so we can get its invoice id.
+				InvoicePaymentBreakDown? payment = dbPaymentBreakDown.Select(invoicePaymentId);
+				
+				if (payment == null || dbPaymentBreakDown.RemovePayment(invoicePaymentId) == false)
+				{// we were unable to remove the payment from the database (may be it does not exists)
 
-				if (dbPaymentBreadkDown.RemovePayment(invoicePaymentId) == false)
-				{
 					returnValue.HasErrors = true;
 					returnValue.Errors.Add("Unable to remove payment");
 					returnValue.ReturnValue = false;
 				}
 				else
-					returnValue.ReturnValue = true;
+				{// payment removed
+					
+
+
+					// Sums up all the payments allocated to this invoice and updates
+					// the TotalInvoiceAmmount value in the Invoice table
+
+					// find the invoice in the database we want to add a payament too
+					InvoiceDb dbInvoice = new InvoiceDb(dbContext);
+					Shared.Models.Database.Invoice.Invoice? FoundInvoice = dbInvoice.SelectInvoice(payment.InvoiceId);
+
+					// make sure we found the invoice
+					if(FoundInvoice != null) 
+					{ 
+						// sum of all the payments allocated to this invoice
+						FoundInvoice.TotalInvoiceAmmount = dbPaymentBreakDown.GetAllPaymentsForInvoice(FoundInvoice.Id).Sum(s => s.Ammount);
+						// update the invoice total ammount in the database
+						dbInvoice.Update(FoundInvoice);
+						dbContext.SaveChanges();
+
+						// set the return value to the ture to say payment was remove
+						// and invoice total ammount has been updated sucsefully
+						returnValue.ReturnValue = true;
+					}
+					// could not find the invoice
+					else
+					{
+						returnValue.HasErrors = true;
+						returnValue.Errors.Add("Payment was removed but Invoice total ammount was not updated");
+						returnValue.ReturnValue = false;
+					}
+
+
+				}
 			}
 
 			return returnValue;
