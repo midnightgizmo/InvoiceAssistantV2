5c840ad7bc6f9475064921a3023c802d9140807c
midnightgizmo <scottamillard@hotmail.com>
2023-03-20 16:11:48 +0000

    feat: Remember search page settings
    localy save view model on search page. If user revisits search page it will remember what search parameters they input from last visiting the page

--- InvoiceAssistantV2/InvoiceAssistantV2/InvoiceAssistantV2/Client/Models/ApplicationSharedData.cs
+++ InvoiceAssistantV2/InvoiceAssistantV2/InvoiceAssistantV2/Client/Models/ApplicationSharedData.cs
@@ -1,4 +1,5 @@
-﻿using InvoiceAssistantV2.Shared.Models.Database.Invoice;
+﻿using InvoiceAssistantV2.Client.ViewModels.Invoices;
+using InvoiceAssistantV2.Shared.Models.Database.Invoice;
 
 namespace InvoiceAssistantV2.Client.Models
 {
@@ -13,5 +14,13 @@ namespace InvoiceAssistantV2.Client.Models
 		/// on the SearchInvoice Page
 		/// </summary>
 		public List<Invoice> InvoicesFromSearchResults { get; set; } = new List<Invoice>();
+
+		/// <summary>
+		/// A view model for the search for invoices page.
+		/// Allows us to keep the users search parameters they enterd so if they navigate
+		/// away from the search page and then go back to it, they don't have to re enter
+		/// all the search parameters again.
+		/// </summary>
+		public SearchInvoiceVM? vmSearch { get; set; }
 	}
 }
