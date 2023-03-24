ea5820c3f38b3d79d600706d5f07b60bcc3a5685
midnightgizmo <scottamillard@hotmail.com>
2023-03-24 10:49:58 +0000

    feat: add CRUD interactions for PaymentDetails table in database

---
+++ InvoiceAssistantV2/InvoiceAssistantV2/Database/DbInteractions/PaymentDetailsDb.cs
@@ -0,0 +1,45 @@
+﻿using InvoiceAssistantV2.Shared.Models.Database.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database.DbInteractions
+{
+	public class PaymentDetailsDb
+	{
+		private Data.InvoiceAssistantDbContext _DbContext;
+		public PaymentDetailsDb(Data.InvoiceAssistantDbContext dbContext)
+		{
+			_DbContext = dbContext;
+		}
+
+		public PaymetDetail? SelectPaymentDetail(int PaymentDetailsId)
+		{
+			return this._DbContext.PaymentDetails.Where(p => p.Id == PaymentDetailsId).FirstOrDefault();
+		}
+
+		public List<PaymetDetail> SelectAllPaymentDetailsRelatingToPaymentMethod(int PaymentMethodId)
+		{
+			return this._DbContext.PaymentDetails.Where(p => p.PaymentMethodId == PaymentMethodId).ToList();
+		}
+
+		public void UpdatePaymentDetails(PaymetDetail paymentDetails) 
+		{
+			this._DbContext.PaymentDetails.Update(paymentDetails);
+			this._DbContext.SaveChanges();
+		}
+
+		public void InsertPaymentDetails(PaymetDetail paymetDetail)
+		{
+			this._DbContext.PaymentDetails.Add(paymetDetail);
+			this._DbContext.SaveChanges();
+		}
+
+		public void DeletePaymentDetails(PaymetDetail paymentDetail)
+		{
+			this._DbContext.PaymentDetails.Remove(paymentDetail);
+		}
+	}
+}
