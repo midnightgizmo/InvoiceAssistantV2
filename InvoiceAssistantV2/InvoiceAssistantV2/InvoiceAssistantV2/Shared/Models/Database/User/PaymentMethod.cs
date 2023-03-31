4f136f0183c6baa0f6100a6bcebd6d25cd5cfa1c
midnightgizmo <scottamillard@hotmail.com>
2023-03-31 14:01:42 +0100

    feat: implemented IEditableObject on PaymentMethod viewmodel

--- InvoiceAssistantV2/InvoiceAssistantV2/InvoiceAssistantV2/Shared/Models/Database/User/PaymentMethod.cs
+++ InvoiceAssistantV2/InvoiceAssistantV2/InvoiceAssistantV2/Shared/Models/Database/User/PaymentMethod.cs
@@ -1,11 +1,13 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace InvoiceAssistantV2.Shared.Models.Database.User
 {
 	/// <summary>
 	/// Holds the details for a type of payment method, e.g. Bank transfer
 	/// </summary>
-	public class PaymentMethod
+	public class PaymentMethod : IEditableObject
 	{
 		[Key]
         public int Id { get; set; }
@@ -21,5 +23,79 @@ namespace InvoiceAssistantV2.Shared.Models.Database.User
 		public string Name { get; set; } = null!;
 
 		public List<PaymetDetail> PaymetDetails { get; set; } = null!;
-    }
+
+
+
+
+
+
+		#region IEditableObject
+
+
+		/// <summary>
+		/// Creates a shallow copy of the <see cref="PaymentMethod"/> class
+		/// </summary>
+		/// <param name="CopyFrom">the object to copy from</param>
+		/// <param name="CopyTo">the object to copy into</param>
+		/// <returns>the copy</returns>
+		private static PaymentMethod Copy(PaymentMethod CopyFrom, PaymentMethod CopyTo)
+		{
+			CopyTo.Id = CopyFrom.Id;
+			CopyTo.Name = CopyFrom.Name;
+			CopyTo.PaymetDetails = CopyFrom.PaymetDetails;
+			CopyTo.UserDetailsId = CopyFrom.UserDetailsId;
+			
+			return CopyTo;
+
+		}
+
+		[NotMapped]
+		public bool IsInEditMode { get; set; } = false;
+
+		/// <summary>
+		/// Used to store a shall copy of this instance of the <see cref="PaymentMethod"/> class
+		/// when we are being edited.
+		/// </summary>
+		[NotMapped]
+		private PaymentMethod? _PaymentMethodShallowCopy { get; set; } = null;
+		public void BeginEdit()
+		{
+			this._PaymentMethodShallowCopy = PaymentMethod.Copy(this, new PaymentMethod());
+			this.IsInEditMode = true;
+		}
+
+		/// <summary>
+		/// We are happy with the changes we made and now want to commit
+		/// them to memory
+		/// </summary>
+		public void EndEdit()
+		{
+			// no longer need the orignal
+			this._PaymentMethodShallowCopy = null;
+			this.IsInEditMode = false;
+		}
+
+		/// <summary>
+		/// We want to restore the orignal values and
+		/// forget any changes that were made from when
+		/// BeginEdit was called
+		/// </summary>
+		public void CancelEdit()
+		{
+			if (this._PaymentMethodShallowCopy == null)
+				return;
+
+			// retore the orignal values
+			PaymentMethod.Copy(this._PaymentMethodShallowCopy, this);
+
+			// no longer need the copy
+			this._PaymentMethodShallowCopy = null;
+
+			this.IsInEditMode = false;
+		}
+
+		#endregion
+
+
+	}
 }
