160b225f3e119610ea111b20fbe0886d13d324ef
midnightgizmo <scottamillard@hotmail.com>
2023-05-24 15:23:58 +0100

    client: create ServerResponseSingleUserAddress
    Holds one instance of a UserAddress Model that will be recieved from the server

---
+++ InvoiceAssistantV2/InvoiceAssistantV2/InvoiceAssistantV2/Client/Models/Server/ResponseData/ServerResponseSingleUserAddress.cs
@@ -0,0 +1,9 @@
+﻿using InvoiceAssistantV2.Shared.Models.Database.User;
+
+namespace InvoiceAssistantV2.Client.Models.Server.ResponseData
+{
+	public class ServerResponseSingleUserAddress : ServerResponseData
+	{
+		public new UserAddress ReturnValue { get; set; }
+	}
+}
